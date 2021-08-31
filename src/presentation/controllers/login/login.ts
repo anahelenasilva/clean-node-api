ddaab4576912b37ab1ddd5772d54ce88f6813612
Ana Helena <anahelenarp@hotmail.com>
2021-08-31 13:20:00 -0300

    refactor: group login controller dependencies on a single file

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,7 @@
-import { Authentication } from '../../../domain/usecases/authentication';
 import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper';
-import { Controller, HttpRequest, HttpResponse } from '../../protocols';
-import { EmailValidator } from '../signup/signup-protocols';
+import { Controller, HttpRequest, HttpResponse } from './login-protocols';
+import { EmailValidator ,Authentication } from './login-protocols';
 
 export class LoginController implements Controller {
 
