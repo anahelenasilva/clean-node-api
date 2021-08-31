a4848432f562aa1174d922fa2599d2570b6a915f
Ana Helena <anahelenarp@hotmail.com>
2021-08-31 12:37:37 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -19,14 +19,12 @@ export class LoginController implements Controller {
     try {
     const { email, password } = httpRequest.body
 
-    if(!email) {
-      return badRequest(new MissingParamError('email'))      
+    const requiredFields = ['email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
-
-    if(!password) {
-      return badRequest(new MissingParamError('password'))      
-    }
-
     
     if(!this.emailValidator.isValid(email)){
       return badRequest(new InvalidParamError('email'))      
