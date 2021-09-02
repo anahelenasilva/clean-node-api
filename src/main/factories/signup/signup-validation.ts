3b1dae8ba424adafddc91133d8b8164d273aa14d
Ana Helena <anahelenarp@hotmail.com>
2021-09-02 13:17:11 -0300

    refactor: group validations

---
+++ src/main/factories/signup/signup-validation.ts
@@ -0,0 +1,17 @@
+
+import { Validation } from '../../../presentation/controllers/signup/signup-protocols'
+import { EmailValidation, CompareFieldsValidation, RequiredFieldValidation } from '../../../presentation/helpers/validators/'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeSignUpValidation = (): ValidationComposite  => {
+
+  const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email',new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
