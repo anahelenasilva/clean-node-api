1b1b49ab4f350b40e1ed095433a9a8d654111864
Ana Helena <anahelenarp@hotmail.com>
2021-09-05 07:22:26 -0300

    test: ensure DbAuthentication retuns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -58,5 +58,14 @@ describe('DbAuthentication UseCase', () => {
     
     await expect(promise).rejects.toThrow();
   });
+  
+  test('Should retun null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve({} as AccountModel)));
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    
+    expect(accessToken).toBe('');
+  });
 
 });
