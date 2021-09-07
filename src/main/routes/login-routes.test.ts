7e3f74434db3895fb468bbf73f7cef8579b1e571
Ana Helena <anahelenarp@hotmail.com>
2021-09-07 14:32:44 -0300

    test: ensure login retusn 401 if no account is found

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -55,5 +55,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'ana@ana.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
