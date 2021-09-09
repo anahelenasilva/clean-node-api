b28f5605f92df00720a32a7e47924cea03f748ca
Ana Helena <anahelenarp@hotmail.com>
2021-09-09 09:43:40 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -5,18 +5,18 @@ import {
   Encrypter,
   LoadAccountByEmailRepository,
   UpdateAccessTokenRepository
-} from "./db-authentication-protocols"
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
-  constructor(
+  constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
     private readonly encrypter: Encrypter,
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
-    ) {
+  ) {
   }
 
-  async auth(authentication: AuthenticationModel): Promise<string> {
+  async auth (authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (account && Object.keys(account).length > 0) {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
@@ -26,9 +26,8 @@ export class DbAuthentication implements Authentication {
         await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
         return new Promise(resolve => resolve(accessToken))
       }
-      
     }
 
-    return new Promise(resolve => resolve(''));
+    return new Promise(resolve => resolve(''))
   }
 }
