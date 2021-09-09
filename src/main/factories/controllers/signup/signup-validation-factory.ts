69b99ee6d40e896a3b7add4cdfd224f70eda2073
Ana Helena <anahelenarp@hotmail.com>
2021-09-09 09:33:44 -0300

    refactor: put factories in folders

---
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -0,0 +1,15 @@
+import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation } from '../../../../presentation/helpers/validators'
+import { ValidationComposite } from '../../../../presentation/helpers/validators/validation-composite'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
