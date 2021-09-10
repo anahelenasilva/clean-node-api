641674cb10c609b69b71e7788ffd8c7b4ecbdb09
Ana Helena <anahelenarp@hotmail.com>
2021-09-10 18:45:43 -0300

    fix: fix addaccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,15 +8,15 @@ import {
 } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor (
+  constructor(
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) { }
 
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
 
-    if (account.name === '') {
+    if (account === null || account.name === '') {
       const hashedPassword = await this.hasher.hash(accountData.password)
       const accountModel = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
       return accountModel
