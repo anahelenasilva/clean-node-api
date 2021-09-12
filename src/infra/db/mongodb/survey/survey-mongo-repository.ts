f22bff792d37f812140d15e16602003a83dc080c
Ana Helena <anahelenarp@hotmail.com>
2021-09-12 20:17:21 -0300

    feat: ensure SurveyMongoRepository calls LoadSurveysRepository

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,19 @@
 
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-protocols'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
+import { SurveyModel } from '../../../../domain/models/survey'
 
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
-  async add (surveyData: AddSurveyModel): Promise<void> {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+  async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys && MongoHelper.map(surveys)
   }
 }
