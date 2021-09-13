1bd8cec18e8dbc05fc2e1ea26306fd231a35dcbb
Ana Helena <anahelenarp@hotmail.com>
2021-09-13 09:05:26 -0300

    fix: fix route

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -8,24 +8,24 @@ import { AddAccountModel } from '../../../../domain/usecases/addAccount'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
-  async add (account: AddAccountModel): Promise<AccountModel> {
+  async add(account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.insertOne(account)
     return MongoHelper.map(account)
   }
 
-  async loadByEmail (email: string): Promise<AccountModel> {
+  async loadByEmail(email: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email })
     return account && MongoHelper.map(account)
   }
 
-  async updateAccessToken (id: string, token: string): Promise<void> {
+  async updateAccessToken(id: string, token: string): Promise<void> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.updateOne({ _id: id }, { $set: { accessToken: token } })
   }
 
-  async loadByToken (token: string, role?: string): Promise<AccountModel> {
+  async loadByToken(token: string, role?: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({
       accessToken: token,
@@ -35,6 +35,14 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
         role: 'admin'
       }]
     })
-    return account && MongoHelper.map(account)
+
+    const acountModel: AccountModel = {
+      email: account?.email,
+      id: account?._id,
+      name: account?.name,
+      password: account?.password,
+    }
+
+    return acountModel
   }
 }
