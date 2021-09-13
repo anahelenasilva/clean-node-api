7657656bd365ef710196ae2bde612c3d0075fa32
Ana Helena <anahelenarp@hotmail.com>
2021-09-13 08:35:11 -0300

    feat: ensure LoadSurveys returns surveys with user access

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware-routes-adapter'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middlewares-factory'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
