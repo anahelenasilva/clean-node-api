4058a3ebbb97b9d3f96f788271689486642040f6
Ana Helena <anahelenarp@hotmail.com>
2021-09-15 08:20:58 -0300

    feat: create mapCollection in mongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,17 +4,17 @@ export const MongoHelper = {
   client: null as unknown as MongoClient,
   url: null as unknown as string,
 
-  async connect (url: string): Promise<void> {
+  async connect(url: string): Promise<void> {
     this.url = url
     this.client = await MongoClient.connect(url)
   },
 
-  async disconnect () {
+  async disconnect() {
     this.client.close()
     this.client = null
   },
 
-  async getCollection (collectionName: string): Promise<Collection> {
+  async getCollection(collectionName: string): Promise<Collection> {
     if (!this.client || !this.client.isConnected) {
       await this.connect(this.url)
     }
@@ -22,8 +22,12 @@ export const MongoHelper = {
     return this.client.db().collection(collectionName)
   },
 
-  map: (collection: any): any => {
-    return Object.assign({}, collection, { id: collection._id })
+  map: (data: any): any => {
+    return Object.assign({}, data, { id: data._id })
+  },
+
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map(c => MongoHelper.map(c))
   }
 
 }
