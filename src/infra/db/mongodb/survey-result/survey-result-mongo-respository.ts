563d1ff5a5cba7479eb4653989ad992b767e4539
Ana Helena <anahelenarp@hotmail.com>
2021-09-15 08:15:17 -0300

    feat: insert SurveyResult

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-respository.ts
@@ -0,0 +1,36 @@
+import { SaveSurveyResultRepository } from "@/data/usecases/save-survey-result/db-save-survey-result-protocols";
+import { SurveyResultModel } from "@/domain/models/survey-result";
+import { SaveSurveyResultModel } from "@/domain/usecases/save-survey-result";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+
+  constructor() { }
+
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveyResults')
+    const res = await surveyCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true
+    })
+
+    const obj = {
+      id: res?.value?._id,
+      surveyId: res?.value?.surveyId,
+      date: res?.value?.date,
+      answer: res?.value?.answer,
+      accountId: res?.value?.accountId
+    }
+
+    console.log('saveResultInserted', obj)
+
+    return obj
+  }
+}
