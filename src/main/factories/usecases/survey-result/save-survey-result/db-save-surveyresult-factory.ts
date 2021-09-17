df7f8574a43d337b54ec710eda59e4111f97da1e
Ana Helena <anahelenarp@hotmail.com>
2021-09-17 09:12:43 -0300

    feat: ensure save survey result return 403 on save surveys result without access token

---
+++ src/main/factories/usecases/survey-result/save-survey-result/db-save-surveyresult-factory.ts
@@ -0,0 +1,8 @@
+import { DbSaveSurveyResult } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result'
+import { SaveSurveyResult } from '@/domain/usecases/save-survey-result'
+import { SurveyResultMongoRepository } from '@/infra/db/mongodb/survey-result/survey-result-mongo-respository'
+
+export const makeDbSaveSurveyResult = (): SaveSurveyResult => {
+  const surveyResultMongoRepository = new SurveyResultMongoRepository()
+  return new DbSaveSurveyResult(surveyResultMongoRepository)
+}
