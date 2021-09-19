25cb6bf943a3ecf4076e778110bb5eb779a0942c
Ana Helena <anahelenarp@hotmail.com>
2021-09-19 09:17:09 -0300

    docs: add docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,35 @@
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Voting System',
-    description: 'A voting system API for developers',
-    version: '1.0.0'
-  }
+    description: 'A voting system API for developers, made with  Typescript, TDD, Clean Architecture e following SOLIDs principles and Design Patterns',
+    version: '1.0.0',
+    contact: {
+      name: 'Ana Helena',
+      email: 'anahelenarp@hotmail.com',
+      url: 'https://www.linkedin.com/in/anahelenasilvasssssss'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    }
+  },
+  servers: [{
+    url: '/api',
+    description: 'Main Server'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'Login releated APIs'
+  }, {
+    name: 'Survey',
+    description: 'Survey related APIs'
+  }],
+  paths,
+  schemas,
+  components
 }
---
+++ src/main/docs/paths/survey-path.ts
@@ -0,0 +1,66 @@
+export const surveyPath = {
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'Get all surveys API',
+    description: 'This route can only be accessed by **authenticated users**',
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveys'
+            }
+          }
+        }
+      },
+      204: {
+        description: 'Success, but no data is shown'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  post: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'Create Survey API',
+    description: 'This route can only be accessed by **admins**',
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/addSurveyParams'
+          }
+        }
+      }
+    },
+    responses: {
+      204: {
+        description: 'Success, but no data is shown'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
