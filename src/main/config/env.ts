1e72e113ab8fae54aa673f21c0b5eca979992c44
Ana Helena <ahelena-magit@vortx.com.br>
2021-09-21 13:46:19 -0300

    fix: remove mongo url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://ana:ana123@cluster0.yzgzr.azure.mongodb.net/voting-system?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL ?? '',
   port: process.env.PORT ?? 5051,
   jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
