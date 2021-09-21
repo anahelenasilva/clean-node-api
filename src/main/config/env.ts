0a7d3b2adeea66fa132ee04639d6243408f041d4
Ana Helena <ahelena-magit@vortx.com.br>
2021-09-21 13:46:49 -0300

    fix: mongo url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? '',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/voting-system',
   port: process.env.PORT ?? 5051,
   jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
