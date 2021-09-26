c6cf7d529482f0fcadc6efbecf3c70b7b020712d
Ana Helena <ahelena-magit@vortx.com.br>
2021-09-26 15:11:49 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws an exception

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,18 +1,22 @@
 import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyResult: LoadSurveyById) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveyResult = await this.loadSurveyResult.loadById(httpRequest.params.surveyId)
-    if (!surveyResult || !surveyResult.id) {
-      return forbidden(new InvalidParamError('surveyId'))
-    }
+    try {
+      const surveyResult = await this.loadSurveyResult.loadById(httpRequest.params.surveyId)
+      if (!surveyResult || !surveyResult.id) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
 
-    return {} as HttpResponse
+      return {} as HttpResponse
+    } catch (error) {
+      return serverError(error)
+    }
   }
 
 }
