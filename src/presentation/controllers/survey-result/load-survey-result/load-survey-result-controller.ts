8da1269df713b1b5e18f7ff0bf285cfded3d0b52
Ana Helena <ahelena-magit@vortx.com.br>
2021-09-26 14:49:44 -0300

    feat: ensure LoadSurveyResultController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,11 +1,17 @@
 import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyResult: LoadSurveyById) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyResult.loadById(httpRequest.params.surveyId)
+    const surveyResult = await this.loadSurveyResult.loadById(httpRequest.params.surveyId)
+    if (!surveyResult || !surveyResult.id) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
+
     return {} as HttpResponse
   }
 
